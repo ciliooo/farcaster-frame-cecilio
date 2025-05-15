a6f67132d900fef9c2da49546d20e2bc97071fa7
ciliooo <cilio270903@gmail.com>
2025-05-15 14:51:32 +0200

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,7 +12,6 @@ export default function App({ Component, pageProps }: AppProps) {
         },
         externalWallets: {
           solana: {},
-          ethereum: {},
         }
       }}
     >
